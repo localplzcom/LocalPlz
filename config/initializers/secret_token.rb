6e3347f5c05fa142f86ebfc6b15e72c58c6691cb
Alex Gerber <agerber23@gmail.com>
2012-02-11 21:03:02 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localplz::Application.config.secret_token = 'ae2212c1dbcfd3433ae35ade8832671b96c9f9e25226f6d1aebe0730567f39efb7e932199040b757cdf5d8b4a5a30687551ac3e6a52b02433a0ffe060a3a5fcb'
