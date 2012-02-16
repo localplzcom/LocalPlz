e97733aa167c7ab1fd2482cfe8e98204e35f8d4d
Alex Gerber <agerber23@gmail.com>
2012-02-16 16:32:14 -0500

    Login works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_to root_path
     else
       @error = 'Incorrect username / password combination.'
       render 'new'
@@ -14,5 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    sign_out
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
+  before_save :create_remember_token
   
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :email, presence: true, format: { with: valid_email_regex }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
