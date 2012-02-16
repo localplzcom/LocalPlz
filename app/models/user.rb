b45ec0aa775cb5e7f08514f30161fbf7c7a709cc
Alex Gerber <agerber23@gmail.com>
2012-02-16 08:26:58 -0500

    User Auth added in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :email
+  attr_accessible :email, :password, :password_confirmation
+  has_secure_password
+  
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :email, presence: true, format: { with: valid_email_regex }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 end
