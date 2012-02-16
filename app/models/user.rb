b120e79b59e83e21af3bf0ef8731fe72342f1ea4
Alex Gerber <agerber23@gmail.com>
2012-02-16 01:59:42 -0500

    Modeled User Email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: valid_email_regex }, uniqueness: { case_sensitive: false }
 end
